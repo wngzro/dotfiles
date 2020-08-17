addd91fdf4a0e88f911169c0166564a4fd7cb914
Joe <wngzro@icloud.com>
2020-08-17 16:42:18 -0500

    Fixing Brewfile path

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Setup symlinks for dotfiles
 ln -s ~/.dotfiles/.zshrc ~
-ln -s ~/.dotfiles/Brewfile ~
+ln -s ~/.dotfiles/.Brewfile ~
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
