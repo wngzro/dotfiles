54edcc352d5ca76e3e8a2fe14574551e27b0ce0d
Joe <wngzro@icloud.com>
2020-08-17 15:11:47 -0500

    Added a few more settings

--- defaults.sh
+++ defaults.sh
@@ -12,6 +12,21 @@ defaults write com.apple.dock autohide -bool true
 # System Preferences > Dock > Show recent application in Dock > Off
 defaults write com.apple.dock show-recents -bool false
 
+# System Preferences > General > Show scroll bars: > When scrolling
+defaults write -g AppleShowScrollBars -string WhenScrolling
+
+# System Preferences > General > Use font smoothing when available > Off
+defaults write -g AppleFontSmoothing -int 0
+
+# System Preferences > General > Highlight color: > Purple
+defaults write -g AppleHighlightColor -string "0.968627 0.831373 1.000000 Purple"
+
+# System Preferences > Keyboard > Text > Correct spelling automatically > Off
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# System Preferences > Keyboard > Text > Capitalize words automatically > Off
+defaults write -globalDomain NSAutomaticCapitalizationEnabled -bool false
+
 # Finder > View > Show Path Bar
 defaults write com.apple.finder ShowPathbar -bool true
 
